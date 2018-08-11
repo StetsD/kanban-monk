55a3028f3adae42a1f24d668194ca08906339d70
StetsD <chortstd@gmail.com>
2018-08-11 13:25:28 +0300

    config init

---
+++ config/index.js
@@ -0,0 +1,28 @@
+module.exports = {
+	host: 'localhost',
+	protocol: 'http',
+	port: 8000,
+	keys: ['naprimer', 'fireblaster'],
+	api: {
+		version: 'v1',
+		name: 'api'
+	},
+	database: {
+		host: 'localhost',
+		port: 27017,
+	},
+	crypto: {
+		hash: {
+			length: 128,
+			iterations: 12000
+		},
+		algorithm: 'sha256'
+	},
+	mailer: {
+		host: 'smtp.yandex.ru',
+		port: 465
+	},
+	paths: {
+		static: '/assets'
+	}
+}
